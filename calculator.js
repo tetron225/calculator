bfc01053b1fd86189d847e6a3bbb91bd6fc6a7d1
tetron <skim6502@gmail.com>
2024-09-24 12:01:59 -0400

    created functions for addition, subtraction, multiplication, and division

--- calculator.js
+++ calculator.js
@@ -4,3 +4,27 @@
 //Create a division function
 
 //create an event listener to help listen in on number keys
+
+const addition = (...num) => {
+    let total = 0;
+    for (const arg of num) {
+        total += arg;
+    }
+    return total;
+}
+
+const subtraction = (num1, num2) => {
+    return Math.abs(num1 - num2);
+}
+
+const multiplication = )(...num) {
+    let total = 0;
+    for(const arg of num) {
+        total *= arg;
+    }
+    return total;
+}
+
+const division = (dividend, divisor) => {
+    return (dividend/divisor);
+}
