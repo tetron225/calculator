d3decaa65eff847838e23b54dd2bb91bab74bfbf
tetron225 <skim6502@gmail.com>
2024-10-01 08:49:56 -0400

    created the operate function to enable what math function to use

--- calculator.js
+++ calculator.js
@@ -17,7 +17,7 @@ const subtraction = (num1, num2) => {
     return Math.abs(num1 - num2);
 }
 
-const multiplication = )(...num) {
+const multiplication = (...num) {
     let total = 0;
     for(const arg of num) {
         total *= arg;
@@ -25,6 +25,26 @@ const multiplication = )(...num) {
     return total;
 }
 
-const division = (dividend, divisor) => {
-    return (dividend/divisor);
+const division = (val1, val2) => {
+    if(val1 > val2) {
+        return(val1/val2);
+    } else {
+        return (val2/val1);
+    }
+}
+
+const operate = (operator, num1, num2) => {
+    let result = 0;
+    switch(operator) {
+        case "+":
+            result = addition(num1, num2);
+        case "-":
+            result = subtraction(num1, num2);
+        case "*":
+            result = multiplication(num1, num2);
+        case "/":
+            result = division(num1, num2);
+        default:
+            console.log("error");
+    }
 }
